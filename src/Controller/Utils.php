4b1411a2834f07bb528a337ea21837ce20600a7b
minhimmaturity <trantanminh0603@gmail.com>
2022-11-18 22:21:17 +0700

    remove user by their id and change user role

--- src/Controller/Utils.php
+++ src/Controller/Utils.php
@@ -9,12 +9,16 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 function findUserId(Request $request, SessionRepository $sessionRepo): int
 {
-    
+    try {
         $sessionId = $request->headers->get('sessionId');
         $session = $sessionRepo->findOneBy(["sessionId" => $sessionId]);
         $userId = $session->getUserId();
-        return $userId;
+        return $userId; 
+    } catch (\Exception $err) {
+       return new JsonResponse (["msg" => $err->getMessage()], 401, []); 
+    }
+         
     
-        // return new JsonResponse (["msg" => $err->getMessage()], 401, []);
+        
     
 }
