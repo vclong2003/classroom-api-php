d2b5d3c422de420742c89ea83942e7701d0fcd6e
vclong2003 <vclong2003@gmail.com>
2022-11-29 16:25:50 +0700

    CRITICAL ERROR, revert

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
